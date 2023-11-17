929b20fec7ac0636ad4258263beacff6a53def6b
Henri du Plessis <henriduplessis99@gmail.com>
2023-11-17 16:35:02 +0200

    Completed atomic transfer, cleaned up

--- atomic_transfer.py
+++ atomic_transfer.py
@@ -1,26 +1,20 @@
 """
 Question 5(ii)
 
-Using the Algorand SDK, write a Python command-line (CLI) based application that demonstrates the atomic transfer capability between 2 accounts A and B. 
-
-In the atomic transfer, demonstrate how account A sends 5 Algos to account B and account B sends 2 units of an Algorand Standard Asset (ASA) to account A.
-
-Notes
-Make use of the Algorand Python SDK and connect to the Algorand Testnet. You can use a public node such as https://testnet-api.algonode.cloud.  
-Create 2 accounts, one for user A and another for user B. 
-Issue an ASA called UCTZAR.
-Fund account A with Algos (you can use a testnet dispenser if you wish e.g. https://bank.testnet.algorand.network) and make sure account B has 10 units of the UCTZAR. 
-Create an atomic transfer in which account A sends 5 Algos to account B and account B sends 2 units of UCTZAR ASA to account A. 
+CLI that demonstrates the atomic transfer capability between 2 accounts on the Algorand testnet 
 """
+
 from algosdk.v2client import algod
 from algosdk import account, transaction
 import json
 import os
 
-algod_address = "https://testnet-api.algonode.cloud"
-algod_token = ""
-algod_client = algod.AlgodClient(algod_token, algod_address)
+algod_client = algod.AlgodClient("", "https://testnet-api.algonode.cloud")
+params = algod_client.suggested_params()
+currency = [ None, "Algos", "UCTZAR" ]
+asset_id = 480136957 
 
+# To create new accounts:
 # account.generate_account()
 # https://bank.testnet.algorand.network/
 
@@ -35,57 +29,6 @@ accounts = [
 	},
 ]
 
-ISSUE_ASA = False
-ATOMIC_TRANSFER = False
-
-sp = algod_client.suggested_params()
-
-if ISSUE_ASA:
-	txn = transaction.AssetConfigTxn(
-		sender=accounts[1]['address'],
-		sp=sp,
-		unit_name="UCTZAR",
-		asset_name="UCTZAR",
-		total=10,
-		strict_empty_address_check=False
-	)
-
-	stxn = txn.sign(accounts[1]['private_key'])
-	txid = algod_client.send_transaction(stxn)
-
-	print(f"Sent asset create transaction with txid: {txid}")
-	# Wait for the transaction to be confirmed
-	results = transaction.wait_for_confirmation(algod_client, txid, 4)
-	print(f"Result confirmed in round: {results['confirmed-round']}")
-
-	created_asset = results["asset-index"]
-	print(f"Asset ID created: {created_asset}")
-
-if ATOMIC_TRANSFER:
-	asset_id = 480136957 
-	opt_in_txn = transaction.AssetTransferTxn(accounts[0]['address'], sp, accounts[0]['address'], 0, index=asset_id)
-	opt_in_txn_2 = transaction.AssetTransferTxn(accounts[1]['address'], sp, accounts[1]['address'], 0, index=asset_id)
-	transaction.assign_group_id([opt_in_txn, opt_in_txn_2])
-	stxn_1 = opt_in_txn.sign(accounts[0]['private_key'])
-	stxn_2 = opt_in_txn_2.sign(accounts[1]['private_key'])
-	signed_group = [stxn_1, stxn_2]
-	tx_id = algod_client.send_transactions(signed_group)
-	result = transaction.wait_for_confirmation(algod_client, tx_id, 4)
-	print(f"Opt-in txID: {tx_id} confirmed in round: {result.get('confirmed-round', 0)}")
-
-
-	asset_amount = 1
-	txn_1 = transaction.PaymentTxn(accounts[1]['address'], sp, accounts[0]['address'], 100)
-	txn_2 = transaction.AssetTransferTxn(accounts[0]['address'], sp, accounts[1]['address'], asset_amount, index=asset_id)
-
-	transaction.assign_group_id([txn_1, txn_2])
-	stxn_1 = txn_1.sign(accounts[1]['private_key'])
-	stxn_2 = txn_2.sign(accounts[0]['private_key'])
-	signed_group = [stxn_1, stxn_2]
-	tx_id = algod_client.send_transactions(signed_group)
-	result = transaction.wait_for_confirmation(algod_client, tx_id, 4)
-	print(f"txID: {tx_id} confirmed in round: {result.get('confirmed-round', 0)}")
-
 def splash_screen():
 	# https://patorjk.com/software/taag/#p=display&f=Slant
 	logo = r"""
@@ -99,12 +42,39 @@ def splash_screen():
 	os.system('cls')
 	print(logo)
 
-
-
 menu = f"""1. View account balances
 2. Execute an atomic transaction
 3. Exit
 """
+
+ISSUE_ASA = False
+OPT_IN = False
+
+if ISSUE_ASA:
+	txn = transaction.AssetConfigTxn(
+		sender=accounts[1]['address'],
+		sp=params,
+		unit_name="UCTZAR",
+		asset_name="UCTZAR",
+		total=10,
+		strict_empty_address_check=False
+	)
+
+	stxn = txn.sign(accounts[1]['private_key'])
+	txid = algod_client.send_transaction(stxn)
+	results = transaction.wait_for_confirmation(algod_client, txid, 4)
+	created_asset = results["asset-index"]
+	print(f"Asset ID created: {created_asset}")
+
+if OPT_IN:
+	opt_in_txn = transaction.AssetTransferTxn(accounts[0]['address'], params, accounts[0]['address'], 0, index=asset_id)
+	opt_in_txn_2 = transaction.AssetTransferTxn(accounts[1]['address'], params, accounts[1]['address'], 0, index=asset_id)
+	transaction.assign_group_id([opt_in_txn, opt_in_txn_2])
+	signed_group = [opt_in_txn.sign(accounts[0]['private_key']), opt_in_txn_2.sign(accounts[1]['private_key'])]
+
+	tx_id = algod_client.send_transactions(signed_group)
+	result = transaction.wait_for_confirmation(algod_client, tx_id, 4)
+	print(f"Opt-in txID: {tx_id} confirmed in round: {result.get('confirmed-round', 0)}")
 
 def output_balances():
 	for i, account in enumerate(accounts):
@@ -115,30 +85,30 @@ def output_balances():
 		print(f"Balance: {info['amount'] / 1e6} Algos  ||  {info['assets'][0]['amount']} UCTZAR")
 		print(f"https://testnet.algoexplorer.io/address/{account['address']}")
 
-currency = [ None, "Algos", "UCTZAR" ]
-
-def atomic_transaction():
-	# Complete transaction and output AlgoExplorer link
-
+def exec_atomic_transaction():
 	accA_currency = int(input("Select Account A cryptocurrency\n1. Algos\n2. UCTZAR\n> "))
-	accA_amount = int(input(f"Enter amount of {currency[accA_currency]} to send\n> "))
+	accA_amount = float(input(f"Enter amount of {currency[accA_currency]} to send\n> "))
 	
 	splash_screen()
 	accB_currency = int(input("Set Account B cryptocurrency\n1. Algos\n2. UCTZAR\n> "))
-	accB_amount = int(input(f"Enter amount of {currency[accB_currency]} to send\n> "))
+	accB_amount = float(input(f"Enter amount of {currency[accB_currency]} to send\n> "))
 
+	if accA_currency == 1:
+		txn_1 = transaction.PaymentTxn(accounts[0]['address'], params, accounts[1]['address'], accA_amount * 1e6)
+	elif accA_currency == 2:
+		txn_1 = transaction.AssetTransferTxn(accounts[0]['address'], params, accounts[1]['address'], accA_amount, index=asset_id)
 
-
-	# txn_1 = transaction.PaymentTxn(accounts[1]['address'], sp, accounts[0]['address'], 100)
-	# txn_2 = transaction.AssetTransferTxn(accounts[0]['address'], sp, accounts[1]['address'], asset_amount, index=asset_id)
-
-	# transaction.assign_group_id([txn_1, txn_2])
-	# stxn_1 = txn_1.sign(accounts[1]['private_key'])
-	# stxn_2 = txn_2.sign(accounts[0]['private_key'])
-	# signed_group = [stxn_1, stxn_2]
-	# tx_id = algod_client.send_transactions(signed_group)
-	# result = transaction.wait_for_confirmation(algod_client, tx_id, 4)
-	# print(f"txID: {tx_id} confirmed in round: {result.get('confirmed-round', 0)}")
+	if accB_currency == 1:
+		txn_2 = transaction.PaymentTxn(accounts[1]['address'], params, accounts[0]['address'], accB_amount * 1e6)
+	elif accB_currency == 2:
+		txn_2 = transaction.AssetTransferTxn(accounts[1]['address'], params, accounts[0]['address'], accB_amount, index=asset_id)
+	
+	transaction.assign_group_id([txn_1, txn_2])
+	signed_group = [txn_1.sign(accounts[1]['private_key']), txn_2.sign(accounts[0]['private_key'])]
+	tx_id = algod_client.send_transactions(signed_group)
+	result = transaction.wait_for_confirmation(algod_client, tx_id, 4)
+	
+	print(f"txID: {tx_id} confirmed in round: {result.get('confirmed-round', 0)}")
 
 
 if __name__ == "__main__":
@@ -155,7 +125,7 @@ if __name__ == "__main__":
 			output_balances()
 			input("\nENTER to return to main menu\n> ")
 		elif option == 2:
-			atomic_transaction()
+			exec_atomic_transaction()
 			input("\nENTER to return to main menu\n> ")
 
 		print()
