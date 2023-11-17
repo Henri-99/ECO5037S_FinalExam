90889e5cea89e4a238f9e299348ae17326edc183
Henri du Plessis <henriduplessis99@gmail.com>
2023-11-17 15:15:24 +0200

    Completed atomic transfer logic

--- atomic_transfer.py
+++ atomic_transfer.py
@@ -36,24 +36,57 @@ accounts = [
 account_info = algod_client.account_info(accounts[0]['address'])
 print(f"Account balance: {account_info.get('amount')} microAlgos")
 
+
+ISSUE_ASA = False
+ATOMIC_TRANSFER = True
+
 sp = algod_client.suggested_params()
-txn = transaction.AssetConfigTxn(
-    sender=accounts[1]['address'],
-    sp=sp,
-    unit_name="UCTZAR",
-    asset_name="UCTZAR",
-    total=10,
-	strict_empty_address_check=False
-)
 
-stxn = txn.sign(accounts[1]['private_key'])
+if ISSUE_ASA:
+	txn = transaction.AssetConfigTxn(
+		sender=accounts[1]['address'],
+		sp=sp,
+		unit_name="UCTZAR",
+		asset_name="UCTZAR",
+		total=10,
+		strict_empty_address_check=False
+	)
 
-txid = algod_client.send_transaction(stxn)
+	stxn = txn.sign(accounts[1]['private_key'])
+	txid = algod_client.send_transaction(stxn)
 
-print(f"Sent asset create transaction with txid: {txid}")
-# Wait for the transaction to be confirmed
-results = transaction.wait_for_confirmation(algod_client, txid, 4)
-print(f"Result confirmed in round: {results['confirmed-round']}")
+	print(f"Sent asset create transaction with txid: {txid}")
+	# Wait for the transaction to be confirmed
+	results = transaction.wait_for_confirmation(algod_client, txid, 4)
+	print(f"Result confirmed in round: {results['confirmed-round']}")
 
-created_asset = results["asset-index"]
-print(f"Asset ID created: {created_asset}")
+	created_asset = results["asset-index"]
+	print(f"Asset ID created: {created_asset}")
+
+if ATOMIC_TRANSFER:
+	asset_id = 480136957 
+	opt_in_txn = transaction.AssetTransferTxn(accounts[0]['address'], sp, accounts[0]['address'], 0, index=asset_id)
+	opt_in_txn_2 = transaction.AssetTransferTxn(accounts[1]['address'], sp, accounts[1]['address'], 0, index=asset_id)
+	transaction.assign_group_id([opt_in_txn, opt_in_txn_2])
+	stxn_1 = opt_in_txn.sign(accounts[0]['private_key'])
+	stxn_2 = opt_in_txn_2.sign(accounts[1]['private_key'])
+	signed_group = [stxn_1, stxn_2]
+	tx_id = algod_client.send_transactions(signed_group)
+	result = transaction.wait_for_confirmation(algod_client, tx_id, 4)
+	print(f"Opt-in txID: {tx_id} confirmed in round: {result.get('confirmed-round', 0)}")
+
+
+	asset_amount = 1
+	txn_1 = transaction.PaymentTxn(accounts[1]['address'], sp, accounts[0]['address'], 100)
+	txn_2 = transaction.AssetTransferTxn(accounts[0]['address'], sp, accounts[1]['address'], asset_amount, index=asset_id)
+
+	transaction.assign_group_id([txn_1, txn_2])
+	stxn_1 = txn_1.sign(accounts[1]['private_key'])
+	stxn_2 = txn_2.sign(accounts[0]['private_key'])
+	signed_group = [stxn_1, stxn_2]
+	tx_id = algod_client.send_transactions(signed_group)
+	result = transaction.wait_for_confirmation(algod_client, tx_id, 4)
+	print(f"txID: {tx_id} confirmed in round: {result.get('confirmed-round', 0)}")
+
+
+
